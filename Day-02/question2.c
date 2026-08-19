9004745c0f2d0603a073363e7e381d872f3ba07d
Pranav-bhasin596 <bhasinpranav483@gmail.com>
2026-08-20 01:47:15 +0530

    Day 02: add two c programs

---
+++ Day-02/question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    float radius, area, circumference;
+    float pi = 3.14159;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = pi * radius * radius;
+    circumference = 2 * pi * radius;
+
+    printf("area = %.2f\n", area);
+    printf("cicumference = %.2f\n", circumference);
+
+    return 0;
+
+}
