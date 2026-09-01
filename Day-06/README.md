fa80ddd5e9f445f2da5b0ef092229622e49eb1e0
Pranav-bhasin596 <bhasinpranav483@gmail.com>
2026-09-01 10:35:30 +0530

    add Day-06 solutions

---
+++ Day-06/README.md
@@ -0,0 +1,7 @@
+# Day 6
+
+## Question 1
+Write a program to input an integer and check whether it is even or odd using if-else.
+
+## Question 2
+Write a program to input an integer and check whether it is positive, negative or zero using nested if-else.
---
+++ Day-06/question2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter an integer: ");
+    scanf("%d", &n);
+    if(n > 0)
+    {
+        printf("positive\n");
+    }
+    else
+    {
+        if (n < 0)
+        {
+printf("negative\n");
+        }
+        else{
+            printf("zero\n");
+        }
+    }
+    return 0;
+}
