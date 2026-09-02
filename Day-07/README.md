14ea4dd7265e3dd3faae174c3a96d5db67321e9e
Pranav-bhasin596 <bhasinpranav483@gmail.com>
2026-09-02 21:11:04 +0530

    add Day-07 solutions

---
+++ Day-07/README.md
@@ -0,0 +1,12 @@
+# Day 07
+
+## Programs
+
+1. Check whether a year is a leap year.
+2. Check whether a character is a vowel or consonant.
+
+## Language
+C
+
+## Author
+Pranav Bhasin
---
+++ Day-08/question1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    char ch;
+
+    printf("enter a character: ");
+    scanf(" %c", &ch);
+if (ch>= 'A' && ch<= 'Z')
+{
+    printf("uppercase alphabet");
+}
+else if (ch >= 'a' && ch <= 'z')
+{
+    printf("lowercase alphabet");
+}
+else if (ch >= '0' && ch <= '9')
+{
+  printf("digit");
+}
+else
+{
+    printf("special character");
+}
+return 0;
+}
